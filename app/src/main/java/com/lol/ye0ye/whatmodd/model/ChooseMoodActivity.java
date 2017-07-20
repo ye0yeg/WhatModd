3f1d454dbc386c2b406de58d136af006797c67b7
ye0yeg <ye0yeg@gmail.com>
2017-07-21 00:32:19 +0800

    半个视图

---
+++ app/src/main/java/com/lol/ye0ye/whatmodd/model/ChooseMoodActivity.java
@@ -0,0 +1,19 @@
+package com.lol.ye0ye.whatmodd.model;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lol.ye0ye.whatmodd.R;
+
+/**
+ * Created by ye0ye on 2017/7/20.
+ */
+
+public class ChooseMoodActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_mood);
+    }
+}
